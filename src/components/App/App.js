a7988f998afa5439847d2ac21424c94d5b83d45c
Matthew Malone <m.malone@me.com>
2019-09-30 19:00:13 -0600

    Import other news data sources

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
-import NewsContainer from '../NewsContainer/NewsContainer'
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
+import NewsContainer from '../NewsContainer/NewsContainer';
 import './App.css';
 
 class App extends Component {
