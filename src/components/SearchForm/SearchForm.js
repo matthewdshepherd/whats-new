7a4563674c61c44e948e744cbfa896f72c24af09
Matthew Malone <m.malone@me.com>
2019-10-02 12:40:09 -0600

    Change SearchForm from a functional component to a class component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-const SearchForm = () => {
-  return (
-    <form className='searchField'>
-      <input className='input' type='text' name='search' placeholder='search'/>
-      <button className='button' >Search</button>
-    </form>
-  )
+class SearchForm extends Component {
+
+ constructor() {
+   super();
+   this.state= {};
+ }
+
+ render() {
+   return (
+     <form className='searchField'>
+       <input className='input' type='text' name='search' placeholder='search' />
+       <button className='button' >Search</button>
+     </form>
+   )
+ }
+
 }
+
 export default SearchForm;
