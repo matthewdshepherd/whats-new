ad7747dbd1495592203f947ceee27418ed8af7f4
Matthew Malone <m.malone@me.com>
2019-10-02 10:09:11 -0600

    Stle News Article Cards

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -6,10 +6,14 @@ import './NewsArticle.css';
 const NewsArticle = ({article}) => {
   return (
     <article className='newsArticle' id={article.id}>
-      <h2>{article.headline}</h2>
-      <img src={article.img} alt={article.headline} />
-      <p>{article.description}</p>
-      <a href={article.url}>Full Story</a>
+      <div className='imageDiv'>
+        <img className='img' src={article.img} alt={article.headline} />
+      </div>
+      <div className='newsArticleContents'>
+        <h2 className='articleTitle' >{article.headline}</h2>
+        <p className='articleDescription' >{article.description}</p>
+        <a className='articleLink' href={article.url}>Full Story > </a>
+      </div>
     </article>
   )
 }
