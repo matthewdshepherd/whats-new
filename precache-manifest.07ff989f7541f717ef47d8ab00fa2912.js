04d4634013e7c57cc4cb22e67f55a202b6917e2a
Matthew Malone <m.malone@me.com>
2019-10-06 19:06:33 -0600

    Updates

---
+++ precache-manifest.07ff989f7541f717ef47d8ab00fa2912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c57fca549796a008cc7eabbd096132",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f95d5a0fed824fe2855c",
+    "url": "./static/css/main.74ebcd61.chunk.css"
+  },
+  {
+    "revision": "5eb911b8a406ce5f667e",
+    "url": "./static/js/2.cffa7557.chunk.js"
+  },
+  {
+    "revision": "f95d5a0fed824fe2855c",
+    "url": "./static/js/main.4760ca75.chunk.js"
+  },
+  {
+    "revision": "a7ba3a89543cc5257511",
+    "url": "./static/js/runtime-main.bf980d17.js"
+  },
+  {
+    "revision": "2f49a783eb91a06ecc061d7fb6139b1d",
+    "url": "./static/media/KnewsLogo.2f49a783.png"
+  }
+]);
